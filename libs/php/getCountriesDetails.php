522245613ed47f024fa510430d9896b28e00b0ec
ionuts94 <64263102+ionuts94@users.noreply.github.com>
2021-02-04 03:50:51 +0200

    Create getCountriesDetails.php

---
+++ libs/php/getCountriesDetails.php
@@ -0,0 +1,19 @@
+<?php
+
+    $url = 'https://restcountries.eu/rest/v2/all';
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_URL,$url);
+
+	$result=curl_exec($ch);
+
+	curl_close($ch);
+
+	$decode = json_decode($result,true);	
+	$output['data'] = $decode;
+
+	header('Content-Type: application/json; charset=UTF-8');
+	echo json_encode($output); 
+	
+?>
